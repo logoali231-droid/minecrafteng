b1754f0d7369086f346aa876f66a4a0ab5fcc1ef
Logoali231 <logoali231@gmail.com>
2026-06-24 11:15:21 -0300

    Delete atualizar_mundo.bat

--- atualizar_mundo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Baixando Mundo do GitHub
-echo Sincronizando arquivos da nuvem...
-git remote set-url origin https://github.com/logoali231-droid/minecrafteras
-git fetch --all
-git reset --hard origin/master
-git pull origin master
-timeout /t 3
-exit
